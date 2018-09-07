8e900d6c3d58dba7e613e8751e061adf38c25eb4
Eddy Tilmant <eddy.tilmant@gmail.com>
2018-09-07 17:27:19 +0200

    corrected some styles

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-
+<style>
+.sentence{
+	text-
+}
+</style>
 </head>
 
 <body>
@@ -20,83 +24,83 @@
 
 <h1 class="page-header">Mots de Fanny et Rebecca</h1>
 <p>
-Fanny: <span class"sentence">Maman tu est belle... on dirais un clown</span><br>
+Fanny: <span class="sentence">Maman tu est belle... on dirais un clown</span><br>
 </p><p>
 Moi: On vas aller bruler t a verrue, ensuite elle vas mourir et elle tombera</span><br>
-Fanny: <span class"sentence">On irra a son enterrement?</span><br>
+Fanny: <span class="sentence">On irra a son enterrement?</span><br>
 </p><p>
 Moi: Ne tape pas sur ta soeur</span><br>
-Fanny: <span class"sentence">Je pensais que c'était un tambour</span><br>
+Fanny: <span class="sentence">Je pensais que c'était un tambour</span><br>
 </p><p>
 Moi: mange
-Fanny: <span class"sentence">Mais ça n'existe pas ce que tu as fait</span><br>
+Fanny: <span class="sentence">Mais ça n'existe pas ce que tu as fait</span><br>
 </p><p>
 Fanny "Tu sais encore courir toi papa?" 0_°</span><br>
 </p><p>
-Fanny: <span class"sentence">"Papaaaa si un zombie carotte nous mange le cerveau on meurt ou bien on devient un zombie?"</span><br>
+Fanny: <span class="sentence">"Papaaaa si un zombie carotte nous mange le cerveau on meurt ou bien on devient un zombie?"</span><br>
 </p><p>
-Fanny: <span class"sentence">Mamy tu as quel âge? </span><br>
+Fanny: <span class="sentence">Mamy tu as quel âge? </span><br>
 M: 50 ans
-Fanny: <span class"sentence">Ouff c’est lourd ça!</span><br>
+Fanny: <span class="sentence">Ouff c’est lourd ça!</span><br>
 </p><p>
-Fanny: <span class"sentence">papa j’ai fait le dessert, (en se relevant de son pot)</span><br>
+Fanny: <span class="sentence">papa j’ai fait le dessert, (en se relevant de son pot)</span><br>
 </p><p>
 Moi: Tu sais que tu aura bientôt 3 ans! </span><br>
-Fanny: <span class"sentence">On les mettra là (en montrant la bibliothèque)</span><br>
+Fanny: <span class="sentence">On les mettra là (en montrant la bibliothèque)</span><br>
 </p><p>
-Fanny: <span class"sentence">il y avais une mamy au travail de papa (notre directrice appréciera :))</span><br>
+Fanny: <span class="sentence">il y avais une mamy au travail de papa (notre directrice appréciera :))</span><br>
 </p><p>
-Fanny: <span class"sentence">Papa je dois vomir (je l’emmène aux toilettes)</span><br>
-Fanny: <span class"sentence">Elle me regarde d’un air étonné, et me dis, mais on ne vomit pas dans les toilettes! On vomit ou alors? </span><br>
-Fanny: <span class"sentence">Par terre!</span><br>
+Fanny: <span class="sentence">Papa je dois vomir (je l’emmène aux toilettes)</span><br>
+Fanny: <span class="sentence">Elle me regarde d’un air étonné, et me dis, mais on ne vomit pas dans les toilettes! On vomit ou alors? </span><br>
+Fanny: <span class="sentence">Par terre!</span><br>
 </p><p>
-Fanny: <span class"sentence">moi aussi je veux pinter (peindre)</span><br>
+Fanny: <span class="sentence">moi aussi je veux pinter (peindre)</span><br>
 </p><p>
-Fanny: <span class"sentence">Je veux voir le feux dentifrice (d’artifice)</span><br>
+Fanny: <span class="sentence">Je veux voir le feux dentifrice (d’artifice)</span><br>
 </p><p>
-Fanny: <span class"sentence">Elle est bonne ta pizza, on dirais que c’est pas toi qui l’a faite</span><br>
+Fanny: <span class="sentence">Elle est bonne ta pizza, on dirais que c’est pas toi qui l’a faite</span><br>
 </p><p>
-Fanny: <span class"sentence">12/2014</span><br>
+Fanny: <span class="sentence">12/2014</span><br>
 Mamy elle vas casser le fauteille</span><br>
 </p><p>
 01/2015<br>
-Fanny: <span class"sentence">Qu’est ce que tu dis papa</span><br>
-Fanny: <span class"sentence">(Air agasé) PAPA IL DIS QUE…. que’est ce que t’a dis papa?</span><br>
+Fanny: <span class="sentence">Qu’est ce que tu dis papa</span><br>
+Fanny: <span class="sentence">(Air agasé) PAPA IL DIS QUE…. que’est ce que t’a dis papa?</span><br>
 </p><p>
  1/2015<br>
-Fanny: <span class"sentence">papa on peux aller au cirque?</span><br>
+Fanny: <span class="sentence">papa on peux aller au cirque?</span><br>
 M: non, pas aujourd'hui</span><br>
-Fanny: <span class"sentence">on ne dis pas non aux enfants, et moi j'emenerais mes enfant au cirque!</span><br>
+Fanny: <span class="sentence">on ne dis pas non aux enfants, et moi j'emenerais mes enfant au cirque!</span><br>
 </p><p>
 M: et tu leur fera quoi a manger? </span><br>
-Fanny: <span class"sentence">des crêpes </span><br>
+Fanny: <span class="sentence">des crêpes </span><br>
 M: tu m'étonnes </span><br>
 </p><p>
 3/2015<br>
-Fanny: <span class"sentence">quand est ce que tu m'achète une robe d'Elsa ?</span><br>
+Fanny: <span class="sentence">quand est ce que tu m'achète une robe d'Elsa ?</span><br>
 Quand tu sera sage!</span><br>
 Ohhhh mais c'est dans longtemps ca!!!</span><br>
 </p><p>
 3/2015<br>
 M: N’appuye pas sur ce bouton </span><br>
-Fanny: <span class"sentence">Papa elle a appuyé</span><br>
-Fanny: <span class"sentence">C’est pas moi c’est moi doigt</span><br>
+Fanny: <span class="sentence">Papa elle a appuyé</span><br>
+Fanny: <span class="sentence">C’est pas moi c’est moi doigt</span><br>
 </p><p>
 4/2014<br>
 M: Dis la vérité, combien de fois t’a fait pipi dans ta culotte?</span><br>
-Fanny: <span class"sentence">(L’air toute gènée) 40 fois!</span><br>
+Fanny: <span class="sentence">(L’air toute gènée) 40 fois!</span><br>
 </p><p>
 6/2015<br>
-Fanny: <span class"sentence">Tu veux un indice sur ton cadeau de fête des père?</span><br>
+Fanny: <span class="sentence">Tu veux un indice sur ton cadeau de fête des père?</span><br>
 Oui</span><br>
-Fanny: <span class"sentence">C’est une tasse</span><br>
+Fanny: <span class="sentence">C’est une tasse</span><br>
 </p><p>
 7/2015<br>
 M: Qui t’a donné une chique?</span><br>
-Fanny: <span class"sentence">Je l’ai trouvée par terre</span><br>
+Fanny: <span class="sentence">Je l’ai trouvée par terre</span><br>
 </p><p>
 10/2015<br>
-Fanny: <span class"sentence">J’ai fait une faute d’orthographe, vous n’avez pas entendu?</span><br>
+Fanny: <span class="sentence">J’ai fait une faute d’orthographe, vous n’avez pas entendu?</span><br>
 On ne fait pas de potes d’orthographe en parlant</span><br>
 Une faute d’othoparlé alors!</span><br>
 </p>

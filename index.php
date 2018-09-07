30b21648892fb5276f94544653627ada4c1007ab
Eddy Tilmant <eddy.tilmant@gmail.com>
2018-09-07 17:01:46 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<h1>Mots de Fanny et Rebecca</h1>
+
+<p>
+B: Maman tu est belle... on dirais un clown<br>
+Moi: On vas aller bruler t a verrue, ensuite elle vas mourir et elle tombera<br>
+F: On irra a son enterrement?<br>
+</p><p>
+Moi: Ne tape pas sur ta soeur<br>
+F: Je pensais que c'était un tambour<br>
+</p><p>
+Moi: mange
+F: Mais ça n'existe pas ce que tu as fait<br>
+</p><p>
+Fanny "Tu sais encore courir toi papa?" 0_°<br>
+</p><p>
+F: "Papaaaa si un zombie carotte nous mange le cerveau on meurt ou bien on devient un zombie?"<br>
+</p><p>
+F: Mamy tu as quel âge? <br>
+M: 50 ans
+F: Ouff c’est lourd ça!<br>
+</p><p>
+B: papa j’ai fait le dessert, (en se relevant de son pot)<br>
+</p><p>
+Moi: Tu sais que tu aura bientôt 3 ans! <br>
+B: On les mettra là (en montrant la bibliothèque)<br>
+</p><p>
+F: il y avais une mamy au travail de papa (notre directrice appréciera :))<br>
+</p><p>
+B: Papa je dois vomir (je l’emmène aux toilettes)<br>
+B: Elle me regarde d’un air étonné, et me dis, mais on ne vomit pas dans les toilettes! On vomit ou alors? <br>
+B: Par terre!<br>
+</p><p>
+B: moi aussi je veux pinter (peindre)<br>
+</p><p>
+B: Je veux voir le feux dentifrice (d’artifice)<br>
+</p><p>
+F: Elle est bonne ta pizza, on dirais que c’est pas toi qui l’a faite<br>
+</p><p>
+R: 12/2014<br>
+Mamy elle vas casser le fauteille<br>
+</p><p>
+01/2015<br>
+F: Qu’est ce que tu dis papa<br>
+R: (Air agasé) PAPA IL DIS QUE…. que’est ce que t’a dis papa?<br>
+</p><p>
+ 1/2015<br>
+R: papa on peux aller au cirque?<br>
+M: non, pas aujourd'hui<br>
+R: on ne dis pas non aux enfants, et moi j'emenerais mes enfant au cirque!<br>
+</p><p>
+M: et tu leur fera quoi a manger? <br>
+R: des crêpes <br>
+M: tu m'étonnes <br>
+</p><p>
+3/2015<br>
+R: quand est ce que tu m'achète une robe d'Elsa ?<br>
+Quand tu sera sage!<br>
+Ohhhh mais c'est dans longtemps ca!!!<br>
+</p><p>
+3/2015<br>
+M: N’appuye pas sur ce bouton <br>
+F: Papa elle a appuyé<br>
+R: C’est pas moi c’est moi doigt<br>
+</p><p>
+4/2014<br>
+M: Dis la vérité, combien de fois t’a fait pipi dans ta culotte?<br>
+R: (L’air toute gènée) 40 fois!<br>
+</p><p>
+6/2015<br>
+B: Tu veux un indice sur ton cadeau de fête des père?<br>
+Oui<br>
+B: C’est une tasse<br>
+</p><p>
+7/2015<br>
+M: Qui t’a donné une chique?<br>
+B: Je l’ai trouvée par terre<br>
+</p><p>
+10/2015<br>
+F: J’ai fait une faute d’orthographe, vous n’avez pas entendu?<br>
+On ne fait pas de potes d’orthographe en parlant<br>
+Une faute d’othoparlé alors!<br>
+</p>
